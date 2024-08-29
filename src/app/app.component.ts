f20f7bd706a4e5635beef28cb3e8c019630201ee
Priyank - PC <priyank.patadiya.3@gmail.com>
2024-08-29 00:04:33 -0400

    add logic for investment calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
-import { InvestmentResultComponent } from "./investment-result/investment-result.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultComponent],
+  imports: [HeaderComponent, UserInputComponent],
 })
 export class AppComponent {}
--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface AnnualData {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+}
 
 @Component({
   selector: 'app-investment-result',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css'
 })
 export class InvestmentResultComponent {
-
+  @Input() annualData!: AnnualData[];
+  
+  constructor(){
+    console.log("controller of investment result");
+    //console.log(this.data);
+    
+  }
 }
