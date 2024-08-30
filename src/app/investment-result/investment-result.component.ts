ffe195045ecfcdeffbb363009b527f878b62d206
Priyank - PC <priyank.patadiya.3@gmail.com>
2024-08-29 22:11:28 -0400

    Revert "add logic for investment calculator"
    This reverts commit f20f7bd706a4e5635beef28cb3e8c019630201ee.

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,28 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-interface AnnualData {
-  year: number;
-  interest: number;
-  valueEndOfYear: number;
-  annualInvestment: number;
-  totalInterest: number;
-  totalAmountInvested: number;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-investment-result',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css'
 })
 export class InvestmentResultComponent {
-  @Input() annualData!: AnnualData[];
-  
-  constructor(){
-    console.log("controller of investment result");
-    //console.log(this.data);
-    
-  }
+
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,64 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { InvestmentResultComponent } from "../investment-result/investment-result.component";
-
-interface AnnualData {
-  year: number;
-  interest: number;
-  valueEndOfYear: number;
-  annualInvestment: number;
-  totalInterest: number;
-  totalAmountInvested: number;
-}
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [CommonModule, FormsModule, InvestmentResultComponent],
+  imports: [],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  constructor(){
-    console.log("controller of user input");
-  }
 
-  
-
-  initialInvestment!: number;
-  annualInvestment!: number;
-  expectedReturn = 5;
-  duration = 10;
-  annualData: AnnualData[] = [];
-
-
-  calculateInvestmentResults() {
-    console.log("let's calculate investment result");
-
-    console.log(this.initialInvestment, this.annualInvestment, this.expectedReturn, this.duration);
-    
-    //const annualData = [];
-    
-    let investmentValue = this.initialInvestment;
-  
-    for (let i = 0; i < this.duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (this.expectedReturn / 100);
-      investmentValue += interestEarnedInYear + this.annualInvestment;
-      const totalInterest =
-        investmentValue - this.annualInvestment * year - this.initialInvestment;
-      this.annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: this.annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: this.initialInvestment + this.annualInvestment * year,
-      });
-    }
-    console.log(this.annualData);
-    
-    //return annualData;
-  }
 }
